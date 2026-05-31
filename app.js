a906f7371a66e382e64e3db0fec49cb1a8a4b997
Rajyalakshmi49 <chundurirajyalakshmi2@gmail.com>
2026-06-01 05:18:45 +0530

    Update API_BASE URL for production environment

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://127.0.0.1:5000/api';/*
+const API_BASE =  API_BASE = 'https://Rajyalakshmi06-diabetrix-backend.hf.space/api';
  * DIABETRIX — app.js  v2.0
  * Features:
  *  - Auth (Login/Signup)
